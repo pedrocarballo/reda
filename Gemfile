e3a5604f444d31873ff0f1c136c2fa317f391334
Juan Maiz <juanmaiz@gmail.com>
2011-12-29 19:45:12 -0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+gem 'json'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+  gem "compass", "~> 0.12.alpha.2"
+end
+
+gem 'jquery-rails'
+gem 'slim'
+gem 'bootstrap-sass', '~> 1.4.3'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reda::Application.config.secret_token = '1875ded5349dbc437e67d0b1bdb25178a76ba8ca01669f43c0c51a96240bf8016f01eb22f711e6841e285a068661dca16ea73063dfb8eeef01a4ec8526e5d961'
