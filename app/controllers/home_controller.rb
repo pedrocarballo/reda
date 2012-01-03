137e64750d9f626f5ec1cc27fc85ce361e2cdfe4
Pedro Axelrud <pedro@softa.com.br>
2012-01-03 16:32:03 -0200

    some changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class HomeController < ApplicationController
   def index
-    @workshops = [
+    @open_workshops = [
       { title: "Ruby1.9", subtitle: "Introdução", info: "Alguma info aqui", dates: "30/1, 1/2 e 3/2", status: [:success, "Inscrições abertas"], level: "" },
       { title: "Ruby1.9", subtitle: "Orientação a Objetos", info: "", dates: "6, 8 e 10/2", status: [:success, "Inscrições abertas"], level: "" },
       { title: "Rails3.1", subtitle: "MVC", info: "", dates: "13, 15 e 16/2", status: [:success, "Inscrições abertas"], level: "" },
@@ -10,10 +10,13 @@ class HomeController < ApplicationController
       { title: "BackboneJS & Jasmine", subtitle: "JS de verdade para suas aplicações", info: "", dates: "12, 14 e 16/3", status: [:success, "Inscrições abertas"], level: "" },
       { title: "Ruby1.9", subtitle: "Metaprogramação", info: "", dates: "19, 21 e 23/3", status: [:success, "Inscrições abertas"], level: "" },
       { title: "Rails3.1", subtitle: "Deployment, escalabilidade & cloud computing", info: "", dates: "26, 28 e 30/3", status: [:success, "Inscrições abertas"], level: "" },
-      { title: "NodeJS1.6", subtitle: "WebSockets com Express Básico", info: "", dates: "2, 4 e 6/4", status: [:success, "Inscrições abertas"], level: "" },
-      { title: "NodeJS1.6", subtitle: "WebSockets com Express Avançado", info: "", dates: "2, 4 e 6/4", status: [:success, "Inscrições abertas"], level: "" },
-      { title: "JaxGL", subtitle: "WebGL para programadores Rails", info: "", dates: "9, 11 e 13/4", status: [:success, "Inscrições abertas"], level: "" },
-      { title: "Vim", subtitle: "Subtitle here!", info: "", dates: "16, 18 e 20/4", status: [:success, "Inscrições abertas"], level: "" }
+    ].map{|w| OpenStruct.new(w) }
+
+    @next_workshops =[
+      { title: "NodeJS1.6", subtitle: "WebSockets com Express Básico", info: "", dates: "2, 4 e 6/4", status: [:notice, "Aguarde"], level: "" },
+      { title: "NodeJS1.6", subtitle: "WebSockets com Express Avançado", info: "", dates: "2, 4 e 6/4", status: [:notice, "Aguarde"], level: "" },
+      { title: "JaxGL", subtitle: "WebGL para programadores Rails", info: "", dates: "9, 11 e 13/4", status: [:notice, "Aguarde"], level: "" },
+      { title: "Vim", subtitle: "Subtitle here!", info: "", dates: "16, 18 e 20/4", status: [:notice, "Aguarde"], level: "" }
     ].map{|w| OpenStruct.new(w) }
   end
 
