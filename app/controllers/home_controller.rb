83261f6f1901e8b06cee2a41ed7317c88dde521e
Pedro Axelrud <pedro@softa.com.br>
2012-01-05 01:16:35 -0200

    payment modal (also not working)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,6 @@
 # encoding: utf-8
-<<<<<<< HEAD
 require 'ostruct'
-=======
 
-require'ostruct'
-
->>>>>>> b53d8a2462f36b17d08c55f6716ae4475d50235a
 class HomeController < ApplicationController
   def index
     @open_workshops = [
